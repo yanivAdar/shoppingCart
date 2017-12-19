69d0b0b06abfd0a8670ed8d71826222054c240a0
yaniv adar <yanivadar@yanivs-iMac.local>
2017-12-19 18:10:33 +0200

    routers part 2

--- server/models/category-model.js
+++ server/models/category-model.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-    name: String,
-    productList: [{type: Schema.Types.ObjectId, ref: 'Product', require: false}]
+    name: {
+        type: String
+    },
+    productList: [{ type: Schema.Types.ObjectId, ref: 'Product' }]
 })
 
 const Category = mongoose.model('Category', CategorySchema);
