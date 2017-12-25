44ddb0559d80edfdeed3cd5a30a9ac6003b09871
yaniv adar <yanivadar@yanivs-iMac.local>
2017-12-25 19:08:46 +0200

    restore and complite changes - product fetching

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategoriesService } from '../../services/categories.service';
 import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-product-list',
@@ -11,18 +14,36 @@ import { Subject } from 'rxjs/Subject';
 export class ProductListComponent implements OnInit {
   currentCategory;
   id;
+  ngUnsubscribe = new Subject<void>()
   constructor(private activatedRoute: ActivatedRoute, private categoriesService: CategoriesService, private router: Router) { }
 
   ngOnInit() {
-    this.categoriesService.categoryId.subscribe(id => {
-      this.id = id;
-      this.categoriesService.getCategory(this.id).subscribe(res => {
-        this.currentCategory = res[0];
-        console.log(this.currentCategory);
-      });
-    });
+    this.categoriesService.categoryId$$
+      .takeUntil(this.ngUnsubscribe)
+      .do((id) => { this.id = id; })
+      .switchMap((id) => {
+        return this.categoriesService.getCategory(this.id);
+      })
+      .map(res => res[0])
+      .subscribe((res) => {
+        this.currentCategory = res;
+      })
   }
 
+  // ngOnInit() {
+  //   this.categoriesService.categoryId$$.subscribe(id => {
+  //     this.id = id;
+  //     this.categoriesService.getCategory(this.id).subscribe(res => {
+  //       this.currentCategory = res[0];
+  //       console.log(this.currentCategory);
+  //     });
+  //   });
+  // }
+
   // this.router.navigate(['../../'], { relativeTo: this.activatedRoute });
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Subject } from 'rxjs/Subject';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Injectable()
 export class CategoriesService {
   categories$$ = new Subject<any>();
   categories;
-  categoryId = new Subject<string>();
+  categoryId$$ = new ReplaySubject<any>(1);
 
   private url = 'http://localhost:4500/categories/';
   constructor(private http: Http) { }
